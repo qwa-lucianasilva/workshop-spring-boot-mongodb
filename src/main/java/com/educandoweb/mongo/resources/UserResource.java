025c8145915d3291a0a3bb43334841880c52eeb5
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-06-01 14:38:22 -0300

    Obtendo um usuario por ID.

--- src/main/java/com/educandoweb/mongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/mongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,6 @@ import com.educandoweb.mongo.services.UserService;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
 	@Autowired
 	private UserService service;
 	
@@ -25,5 +25,11 @@ public class UserResource {
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
---
+++ src/main/java/com/educandoweb/mongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.educandoweb.mongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
