8bfda5d192843e79ccb4433d0224c4cf57d73b77
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-06-01 15:02:08 -0300

    Inserção de usuario com POST

--- src/main/java/com/educandoweb/mongo/services/UserService.java
+++ src/main/java/com/educandoweb/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.mongo.domain.User;
+import com.educandoweb.mongo.dto.UserDTO;
 import com.educandoweb.mongo.repository.UserRepository;
 import com.educandoweb.mongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);	
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(),objDTO.getEmail());
 	}
 }
