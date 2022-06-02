808f7dfa309adbadfcd551e4b24573dc35e3b1f9
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-06-02 16:24:56 -0300

    Referenciando os posts do usuario

--- src/main/java/com/educandoweb/mongo/config/Instantiation.java
+++ src/main/java/com/educandoweb/mongo/config/Instantiation.java
@@ -18,29 +18,33 @@ import com.educandoweb.mongo.repository.UserRepository;
 public class Instantiation implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userReposiroty;
-	
+	private UserRepository userRepository;
+
 	@Autowired
 	private PostRepository postRepository;
 
 	@Override
 	public void run(String... arg0) throws Exception {
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-
-		userReposiroty.deleteAll();
-
+		
+		userRepository.deleteAll();
+		postRepository.deleteAll();
+		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
-		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-		
+
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
