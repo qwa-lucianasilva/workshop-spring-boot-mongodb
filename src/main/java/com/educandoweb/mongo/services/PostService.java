c6d963a3eba8d00203133aec97d4a9618bf14616
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-06-03 12:14:15 -0300

    Obtendo um post por id

---
+++ src/main/java/com/educandoweb/mongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.educandoweb.mongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.mongo.domain.Post;
+import com.educandoweb.mongo.repository.PostRepository;
+import com.educandoweb.mongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
